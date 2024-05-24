d0ab11408d1a7c133a8af765c549af2cbc2961ad
ManuVerger <mverger@frba.utn.edu.ar>
2024-05-24 10:29:00 -0300

    act webapp

--- src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
+++ src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
@@ -5,18 +5,27 @@ import io.javalin.Javalin;
 
 public class WebApp {
     public static void main(String[] args) {
+        var env = System.getenv();
+
+        // Variables de entorno
+        var URL_VIANDAS = env.get("URL_VIANDAS");
+        var URL_HELADERAS = env.get("URL_HELADERAS");
+
         Fachada fachada = new Fachada();
-        Integer port = Integer.parseInt(System.getProperty("port", "8080"));
+
+        Integer port = Integer.parseInt(env.getOrDefault("PORT", "8080"));
+
         Javalin app = Javalin.create().start(port);
+
         app.get("/", ctx -> ctx.result("Servicio de Viandas!!!"));
         app.post("/viandas", new AltaViandaController(fachada)); // Postea una vianda
         app.get("/viandas/{qr}", new ListaViandaQRController(fachada)); // Devuelve vianda por QR y mensaje 404 si no encuentra
         app.get("/viandas/{qr}/vencida", new ListaViandaVencidaController(fachada)); // Devuelve true si la vianda está vencida, false si no se encuentra o no está vencida
         app.patch("/viandas/{qr}", new ModificarHeladeraController(fachada)); // Modifica UNICAMENTE la heladeraId de una vianda por QR mediante un PATCH
         app.get("/viandas/search/findByColaboradorIdAndAnioAndMes", new ViandasColaboradorController(fachada)); // Devuelve las viandas de un colaborador para un mes y año especificados
+
     }
 }
-
 
 //para probar en postman
 //    {
