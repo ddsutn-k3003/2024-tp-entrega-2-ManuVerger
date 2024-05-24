19d874616abaeaaee354293485eff2d9efc5c617
ManuVerger <mverger@frba.utn.edu.ar>
2024-05-24 10:36:35 -0300

    act controlador vencimiento

--- src/main/java/ar/edu/utn/dds/k3003/controllers/ListaViandaVencidaController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controllers/ListaViandaVencidaController.java
@@ -1,11 +1,11 @@
 package ar.edu.utn.dds.k3003.controllers;
 
 import ar.edu.utn.dds.k3003.app.Fachada;
-import ar.edu.utn.dds.k3003.facades.dtos.EstadoViandaEnum;
-import ar.edu.utn.dds.k3003.facades.dtos.ViandaDTO;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.json.JSONObject;
+
+import java.util.NoSuchElementException;
 
 public class ListaViandaVencidaController implements Handler {
     private Fachada fachada;
@@ -21,13 +21,48 @@ public class ListaViandaVencidaController implements Handler {
         JSONObject resultado = new JSONObject();  // Objeto JSON para el resultado
 
         try {
-            ViandaDTO vianda = fachada.buscarXQR(qr);
-            boolean viandaVencida = vianda.getEstado().equals(EstadoViandaEnum.VENCIDA);
+            boolean viandaVencida = fachada.evaluarVencimiento(qr);
             resultado.put("resultado", viandaVencida);  // Agregar resultado al objeto JSON
             ctx.result(resultado.toString()).contentType("application/json");
-        } catch (IllegalArgumentException e) {
+        } catch (NoSuchElementException e) {
             resultado.put("resultado", false);  // Si no se encuentra la vianda, devuelve falso
             ctx.status(404);
         }
     }
 }
+
+
+// ver estado de la vianda en vez de usar evaluarvencimiento.
+//package ar.edu.utn.dds.k3003.controllers;
+//
+//import ar.edu.utn.dds.k3003.app.Fachada;
+//import ar.edu.utn.dds.k3003.facades.dtos.EstadoViandaEnum;
+//import ar.edu.utn.dds.k3003.facades.dtos.ViandaDTO;
+//import io.javalin.http.Context;
+//import io.javalin.http.Handler;
+//import org.json.JSONObject;
+//
+//public class ListaViandaVencidaController implements Handler {
+//    private Fachada fachada;
+//
+//    public ListaViandaVencidaController(Fachada fachada) {
+//        this.fachada = fachada;
+//    }
+//
+//    @Override
+//    public void handle(Context ctx) throws Exception {
+//        String qr = ctx.pathParam("qr");
+//
+//        JSONObject resultado = new JSONObject();  // Objeto JSON para el resultado
+//
+//        try {
+//            ViandaDTO vianda = fachada.buscarXQR(qr);
+//            boolean viandaVencida = vianda.getEstado().equals(EstadoViandaEnum.VENCIDA);
+//            resultado.put("resultado", viandaVencida);  // Agregar resultado al objeto JSON
+//            ctx.result(resultado.toString()).contentType("application/json");
+//        } catch (IllegalArgumentException e) {
+//            resultado.put("resultado", false);  // Si no se encuentra la vianda, devuelve falso
+//            ctx.status(404);
+//        }
+//    }
+//}
